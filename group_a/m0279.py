a148ed60edab9e60138febefc4234f732e09214a
Daniyar <daniyar.bakir@gmail.com>
2022-03-29 01:02:07 +0600

    added changes

--- group_a/m0279.py
+++ group_a/m0279.py
@@ -1,44 +1,24 @@
-import math
 class Solution:
     def numSquares(self, n: int) -> int:
+        seen_squares = []
+        jj = 1
 
-        def traverse(start_idx, current_sum, n, possible_squares, current_count, best_count):
+        memo = [0] * (n + 1)
 
-            if current_count > best_count[0]:
-                return -1
+        for ii in range(1, n+1):
+            if ii == jj**2:
+                seen_squares.append(jj**2)
+                memo[ii] = 1
+                jj += 1
+                continue
 
-            if current_sum > n:
-                return
-            elif current_sum == n:
-                best_count[0] = min(best_count[0], current_count)
-                return -1
+            memo[ii] = min([memo[sq] + memo[ii-sq] for sq in seen_squares])
 
-            for idx in range(start_idx, len(possible_squares)):
-                val = traverse(idx, current_sum + possible_squares[idx], n, possible_squares, current_count + 1, best_count)
-                if val == -1:
-                    break
-
-            return
-
-        possible_squares = [x**2 for x in reversed(range(math.floor(math.sqrt(n))+1))]
-
-        best_count = [n]
-        print(f'possible_squares: {possible_squares}')
-
-        traverse(0, 0, n, possible_squares, 0, best_count)
-
-        return best_count[0]
+        return memo[-1]
 
 
 if __name__ == "__main__":
     n = 12
-    n = 13
-    n = 36
-    n = 29
-    n = 39
-
-    n = 13
-
 
     sol = Solution()
     print(sol.numSquares(n))
